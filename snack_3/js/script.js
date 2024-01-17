185d0a120cafd60de620da4a70a0c6eb7b3647e7
AlessiaOsman <alessia.osman@gmail.com>
2024-01-17 18:35:18 +0100

    first snack

---
+++ snack_3/js/script.js
@@ -0,0 +1,43 @@
+/* Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e peso.
+Stampare in console la bici con peso minore utilizzando il destructuring*/
+
+console.log('js ok')
+
+const racingBikes = [
+    { 
+        name: "Bianchi Oltre XR4", 
+        weight: 7.0 
+    },
+    { 
+        name: "Specialized Tarmac SL7",
+        weight: 6.7 
+    },
+    { 
+        name: "Canyon Ultimate CF SLX", 
+        weight: 7.1 
+    },
+    { 
+        name: "Trek Madone SLR 9", 
+        weight: 7.2 
+    }
+];
+
+
+
+
+const lowerWeightBike = racingBikes.reduce((result, bike)=>{
+
+let lowestWeight = result.weight < bike.weight ? result : bike
+
+return lowestWeight
+    
+}, racingBikes[0])
+
+console.log(lowerWeightBike)
+
+
+
+
+
+
